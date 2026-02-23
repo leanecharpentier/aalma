83be006bf06362496c9382a526989863ba91ab3b
Mahora Grolleau <hiphopfraise@gmail.com>
2026-02-23 21:55:07 +0100

    Feat (Company) : CRUD + fil table roles

--- back/src/company/company.service.ts
+++ back/src/company/company.service.ts
@@ -1,32 +1,60 @@
-import { Injectable } from '@nestjs/common';
-import { CreateCompanyDto } from './dto/create-company.dto';
-import { UpdateCompanyDto } from './dto/update-company.dto';
-import { Company } from 'typeorm/entities/Company';
-import { AppDataSource } from 'DataSource';
+import { Injectable } from "@nestjs/common";
+import { CreateCompanyDto } from "./dto/create-company.dto";
+import { UpdateCompanyDto } from "./dto/update-company.dto";
+import { Company } from "typeorm/entities/Company";
+import { AppDataSource } from "DataSource";
 
 @Injectable()
 export class CompanyService {
-  create(createCompanyDto: CreateCompanyDto) {
-    return 'This action adds a new company';
+  async create(createCompanyDto: CreateCompanyDto) {
+    return await AppDataSource.getRepository(Company)
+      .createQueryBuilder("company")
+      .insert()
+      .values(createCompanyDto)
+      .execute();
   }
 
-  findAll() {
-    return `This action returns all company`;
+  /**
+   * Return all companies in the database.
+   * @returns Company[]
+   */
+  async findAll(): Promise<Company[]> {
+    return await AppDataSource.getRepository(Company)
+      .createQueryBuilder("company")
+      .getMany();
   }
 
+  /**
+   * Return a company with the given id.
+   * @param {number} id Company id
+   * @returns Company | null
+   */
   async findOne(id: number): Promise<Company | null> {
-    return await AppDataSource
-    .getRepository(Company)
-    .createQueryBuilder("company")
-    .where("company.id = :id", { id: id })
-    .getOne();
+    return await AppDataSource.getRepository(Company)
+      .createQueryBuilder("company")
+      .where("company.id = :id", { id: id })
+      .getOne();
   }
 
-  update(id: number, updateCompanyDto: UpdateCompanyDto) {
-    return `This action updates a #${id} company`;
+  async update(id: number, updateCompanyDto: UpdateCompanyDto) {
+    return await AppDataSource.getRepository(Company)
+      .createQueryBuilder("company")
+      .update()
+      .set(updateCompanyDto)
+      .where("company.id = :id", { id: id })
+      .execute();
   }
 
-  remove(id: number) {
-    return `This action removes a #${id} company`;
+  /**
+   * Delete a company with the given id.
+   * @param {number} id Company id
+   * @returns DeleteResult
+   */
+  async remove(id: number) {
+    return await AppDataSource.getRepository(Company)
+      .createQueryBuilder("company")
+      .delete()
+      .where("company.id = :id", { id: id })
+      .execute();
   }
 }
