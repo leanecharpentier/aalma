2b4964e8c3d332efb29c37195c37cb2fce124da8
Mahora Grolleau <hiphopfraise@gmail.com>
2026-04-15 13:58:49 +0200

    qdishgimp

--- front/middleware.ts
+++ front/middleware.ts
@@ -42,6 +42,12 @@ export async function middleware(request: NextRequest) {
   return NextResponse.next();
 }
 
+// export const config = {
+//   matcher: ["/((?!_next/static|_next/image|favicon.ico|images|.*\\..*).*)"],
+// };
+
 export const config = {
-  matcher: ["/((?!_next/static|_next/image|favicon.ico|images|.*\\..*).*)"],
+  matcher: [
+    "/((?!_next/static|_next/image|favicon.ico|images|api/debug-session|.*\\..*).*)",
+  ],
 };
